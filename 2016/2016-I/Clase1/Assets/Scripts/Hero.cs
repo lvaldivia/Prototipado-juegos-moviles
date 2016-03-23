ee093d0990b76b98ec80d69c1ab4bfcacce499f9
Profesor <Profesor>
2016-03-22 21:33:55 -0500

    clase 2016

---
+++ 2016/2016-I/Clase1/Assets/Scripts/Hero.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	int dir  = -1;
+	public float speed;
+	public float bounce;
+	public float increase;
+	float tmpBounce;
+	float scaleX;
+	void Start () {
+		tmpBounce = 1;
+		scaleX = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*Vector3 position = transform.position;
+		if (position.x > 5 || position.x < -5) {
+			dir *= -1;
+			scaleX *= -1;
+			transform.localScale = new Vector3 (scaleX,
+				transform.localScale.y,
+				transform.localScale.z);
+			//tmpBounce++;
+		}
+		if (tmpBounce % bounce == 0) {
+			speed += increase;
+		}
+		position.x += speed*dir*Time.deltaTime;
+		transform.position = position;*/
+
+	}
+}
---
+++ 2016/2016-I/Clase1/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		if(Input.GetKey(KeyCode.A)){
+			position.x -= speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			position.x += speed*Time.deltaTime;
+		}
+
+		transform.position = position;
+	}
+}
