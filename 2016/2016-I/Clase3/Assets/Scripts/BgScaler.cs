fcbac461574fafce937c51db801240422dba1a87
Alumno-J <Alumno-J>
2016-04-05 21:31:06 -0500

    clase3

---
+++ 2016/2016-I/Clase3/Assets/Scripts/BgScaler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgScaler : MonoBehaviour {
+
+	void Start () {
+		float height = Camera.main.orthographicSize * 2f;
+		float width = height * Screen.width / Screen.height;
+		if (gameObject.name == "background") {
+			transform.localScale = new Vector3 (width, height, 0);	
+		}else if(gameObject.name =="floor"){
+			transform.localScale = new Vector3 (width, 5f, 0);
+		}
+	}
+
+}
---
+++ 2016/2016-I/Clase3/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerJump : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D body;
+	bool canJump;
+	Button jumpButton;
+	Animator anim;
+	float jumpPower = 12f;
+	const string RUNNING = "running";
+	const string JUMPING = "jumping";
+	const string IDLE = "iddle";
+
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		jumpButton = GameObject.Find ("jumpStart").GetComponent<Button>();
+		anim = GetComponent<Animator> ();
+		anim.Play (RUNNING);
+		jumpButton.onClick.AddListener (() => jump ());
+		canJump = false;
+	}
+
+	void jump(){
+		if (canJump) {
+			canJump = false;
+			anim.Play (JUMPING);
+			body.velocity = new Vector2 (body.velocity.x, jumpPower);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs (body.velocity.y) == 0) {
+			canJump = true;
+			anim.Play (RUNNING);
+		}
+	}
+}
