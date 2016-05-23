e48b73223a4e169b59f6b81a6dcad4ac8d60b60d
Luis <valdivialuis1989@gmail.com>
2016-05-23 10:29:05 -0500

    fix

---
+++ 2016/2016-I/Clase7/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	public Button starButton, optionsButton, quitButton, highScoreButton;
+	void Start () {
+		starButton.onClick.AddListener (() => goGame ());
+		optionsButton.onClick.AddListener (() => goOptions ());
+		quitButton.onClick.AddListener (() => goQuit ());
+		highScoreButton.onClick.AddListener (() => goHighScore ());
+	}
+	
+	void goGame(){
+		SceneManager.LoadScene ("Game");
+	}
+
+	void goOptions(){
+		SceneManager.LoadScene ("Options");	
+	}
+
+	void goQuit(){
+		Application.Quit ();
+	}
+
+	void goHighScore(){
+		SceneManager.LoadScene ("HighScore");	
+	}
+}
