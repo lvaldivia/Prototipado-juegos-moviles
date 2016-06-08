3cbf420f6d4986f9d29d5133637a9292d46c5938
Alumno-J <Alumno-J>
2016-06-07 22:13:06 -0500

    prot

---
+++ 2016/2016-I/Clase11/Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Button share;
+	public string TWITTER_INTENT = "http://twitter.com/intent/tweet";
+	public string TWITTER_LANG = "en";
+	void Start () {
+		share.onClick.AddListener (() => shareTwitter ());
+	}
+
+	void shareTwitter(){
+		Application.OpenURL(TWITTER_INTENT+"?text="+WWW.EscapeURL("HOLI :D")+
+			"&amp;lang="+WWW.EscapeURL(TWITTER_LANG));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
